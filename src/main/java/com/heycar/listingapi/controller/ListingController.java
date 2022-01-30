4f6459731ee29310caa3f1de029abdcd09a15442
Özcan Candağ <ozcan.candag@192.168.1.101>
2022-01-30 14:03:04 +0300

    closes #2

--- src/main/java/com/heycar/listingapi/controller/ListingController.java
+++ src/main/java/com/heycar/listingapi/controller/ListingController.java
@@ -4,6 +4,7 @@ import com.heycar.listingapi.model.dto.ListingDto;
 import com.heycar.listingapi.model.request.SearchListingRequest;
 import com.heycar.listingapi.model.response.ListingResponse;
 import com.heycar.listingapi.service.ListingService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -23,24 +24,28 @@ public class ListingController {
 
     private final ListingService listingService;
 
+    @Operation(summary = "Upload new listings for a dealer in JSON array format")
     @PostMapping("/{dealerId}")
     public ResponseEntity upsertListing(@PathVariable Long dealerId, @Valid @RequestBody List<ListingDto> listings) {
         listingService.upsertListing(dealerId, listings);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Upload new listings for a dealer in CSV format")
     @PostMapping(value = "/csv/{dealerId}")
     public ResponseEntity upsertListingCSV(@PathVariable Long dealerId, @RequestParam("file") MultipartFile file) {
         listingService.upsertListingCSV(dealerId, file);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Get listings by dealer")
     @GetMapping("/{dealerId}")
     public ResponseEntity<List<ListingDto>> getListings(@PathVariable Long dealerId) {
         var listings = listingService.getListings(dealerId);
         return ResponseEntity.ok(listings);
     }
 
+    @Operation(summary = "Search listings with make, model, year, color")
     @GetMapping("/search")
     public ResponseEntity<List<ListingResponse>> searchListing(@Valid SearchListingRequest request) {
         var listings = listingService.searchListings(request);
